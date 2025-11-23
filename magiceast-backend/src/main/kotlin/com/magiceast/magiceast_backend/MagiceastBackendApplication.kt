49ed73905c6626cce533db5bfecf289e75fa1f8e
Agustin Morales <ag.moralesp@duocuc.cl>
2025-11-23 13:06:52 -0300

    Creacion Proyecto

---
+++ magiceast-backend/src/main/kotlin/com/magiceast/magiceast_backend/MagiceastBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.magiceast.magiceast_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagiceastBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MagiceastBackendApplication>(*args)
+}
---
+++ magiceast-backend/src/test/kotlin/com/magiceast/magiceast_backend/MagiceastBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.magiceast.magiceast_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MagiceastBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
